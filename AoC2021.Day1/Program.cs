c5bb7ee628fa61d5450ac07fb805e2913342a51d
George Davis <gtdavis25@gmail.com>
2021-12-01 08:29:08 +0000

    Completed day 1

---
+++ AoC2021.Day1/Program.cs
@@ -0,0 +1,30 @@
+﻿var input = File.ReadAllLines(args[0]).Select(int.Parse).ToArray();
+var count = 0;
+for (int i = 1; i < input.Length; i++)
+{
+    if (input[i] > input[i - 1])
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Part 1: {count}");
+var windows = new int[input.Length - 2];
+for (int i = 0; i < windows.Length; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        windows[i] += input[i + j];
+    }
+}
+
+count = 0;
+for (int i = 1; i < windows.Length; i++)
+{
+    if (windows[i] > windows[i - 1])
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Part 2: {count}");
