ae92ce516e4b5fb35e02453260973e14a2b02705
George Davis <gtdavis25@gmail.com>
2021-12-07 18:23:49 +0000

    Completed day 7

---
+++ AoC2021.Day7/Program.cs
@@ -0,0 +1,19 @@
+﻿var crabs = File
+    .ReadAllText(args[0])
+    .Split(',')
+    .Select(int.Parse)
+    .ToArray();
+
+var min = crabs.Min();
+var max = crabs.Max();
+var result = Enumerable
+    .Range(min, max - min + 1)
+    .Min(x => crabs.Sum(crab => Math.Abs(crab - x)));
+
+Console.WriteLine($"Part 1: {result}");
+
+result = Enumerable
+    .Range(min, max - min + 1)
+    .Min(x => crabs.Sum(crab => Math.Abs(crab - x) * (Math.Abs(crab - x) + 1) / 2));
+
+Console.WriteLine($"Part 2: {result}");
